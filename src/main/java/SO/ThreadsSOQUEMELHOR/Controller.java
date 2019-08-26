101a89d80f04d6b7cb69f708fe492344442df6be
Charioteer <49540020+nv343@users.noreply.github.com>
2019-08-26 16:23:36 -0300

    Oi

--- src/main/java/SO/ThreadsSOQUEMELHOR/Controller.java
+++ src/main/java/SO/ThreadsSOQUEMELHOR/Controller.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 
 public class Controller {
@@ -17,11 +18,11 @@ public class Controller {
 	@FXML
 	private Button startClock;
 	@FXML
-	private static Text hourC;
+	private Text hourC;
 	@FXML
-	private static Text minuteC;
+	private Text minuteC;
 	@FXML
-	private static Text secondC;
+	private Text secondC;
 	@FXML
 	private TextField getHour;
 	@FXML
@@ -31,15 +32,13 @@ public class Controller {
 	@FXML
 	private Button botao;
 	@FXML
-	static Label teste;
+	private Button check;
 
 	@FXML
 	private void startClock() throws InterruptedException {
 		Clock c = new Clock();
 		c.start();
-		Teste t = new Teste();
-		t.start();
-
+		showTime();
 	}
 
 	@FXML
@@ -58,16 +57,36 @@ public class Controller {
 	}
 
 	@FXML
-	private void adjustTime() {
+	private void adjustTime() throws InterruptedException {
 
+		if (getHour.getText().equals("")|| getMinute.getText().equals("")|| getSecond.getText().equals("")) {
+			System.out.println("ERRO.NULL");
+			return;
+		} else {
+
+			int a = Integer.parseInt(getHour.getText());
+			int b = Integer.parseInt(getMinute.getText());
+			int c = Integer.parseInt(getSecond.getText());
+			if (a < 0 || a >= 24) {
+				System.out.println("ERRO.HOUR");
+			} else if (b < 0 || b >= 60) {
+				System.out.println("ERRO.MINUTE");
+			} else if (c < 0 || c >= 60) {
+				System.out.println("ERRO.SECOND");
+			} else {
+				time.hh = a;
+				time.mm = b;
+				time.ss = c;
+				showTime();
+			}
+		}
 	}
 
 	@FXML
-	private void teste() {
+	private void showTime() {
+		hourC.setText(Integer.toString(time.hh));
+		minuteC.setText(Integer.toString(time.mm));
+		secondC.setText(Integer.toString(time.ss));
 
-	}
-
-	public static void setTeste(String teste) {
-		Controller.teste.setText(teste);;
 	}
 }
